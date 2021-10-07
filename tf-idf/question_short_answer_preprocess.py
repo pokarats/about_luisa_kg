1d6a4457adb644a53e664454bed983f88cef3051
Daria Fedorova <dariafedorovav@gmail.com>
2021-10-07 23:07:23 +0200

    move files from one place to other

--- tf-idf/question_short_answer_preprocess.py
+++
@@ -1,8 +0,0 @@
-
-with open('question_short_answer.txt', 'w', encoding="ISO-8859-1") as writer:
-    with open("qa_extended_pair.txt", 'r', encoding="ISO-8859-1") as f:
-        for line in f:
-            line = line.strip()
-            line = line.split("<start extention>")
-            question_short_answer = line[0]
-            writer.write(question_short_answer + "\n")
