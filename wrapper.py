3b8c240d7172b64c7e41112c454ac18c316c65cb
Daria Fedorova <dariafedorovav@gmail.com>
2021-11-21 00:01:06 +0300

    update wrapper

---
+++ wrapper.py
@@ -0,0 +1,25 @@
+'''
+This wrapper aims to intergrate the Answer Extension module into
+the overall pipeline. We receive as an input a question-answer pair retreived by the
+Q-A Chatbot module, then extend and paraphrase the answer if appropriate.
+'''
+
+from extension.tf_idf import tf_idf
+from extension.t5_model_paraphrase import t5_model_paraphrase
+
+# it is just an example
+#question_answer = "What inspires your art? A lot, experiences, texts, songs, films, conversations, observations, the daily life as news."
+question_answer = "Are you interested in architecture? Yes I am very interested in architecture"
+
+# TODO: classify the answer
+
+# if the Question Answer pair is classified as extendable:
+# generate an extension with tf-idf model and paraphrase it with T5
+tf_idf_extension = tf_idf.response(question_answer)
+print(tf_idf_extension)
+#pass the tf_idf output to the paraphrasing module
+#TODO: or should we make paraphrasing optional?
+extension_paraphrased = t5_model_paraphrase.paraphrase(tf_idf_extension)
+print(extension_paraphrased)
+
+
