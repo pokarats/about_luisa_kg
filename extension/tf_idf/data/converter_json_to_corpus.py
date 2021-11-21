4d45cac9c7b147f246570db1295fbf637f68ade6
Daria Fedorova <dariafedorovav@gmail.com>
2021-11-22 00:03:40 +0300

    move files

---
+++ extension/tf_idf/data/converter_json_to_corpus.py
@@ -0,0 +1,32 @@
+# This module converts the final filtered Louisa courpus in the json format to simple txt
+import json
+input_file = "corpus_tagged.json"
+output_file = "corpus_final.txt"
+
+def converter(fin, fout):
+    line_list=[]
+    with open(fin, "r", encoding="utf8") as fin:
+        # read the file and lower case
+        loaded_file=json.load(fin)
+        for item_dict in loaded_file:
+            question_line=""
+            if "question" in item_dict:
+                question_line=item_dict["question"] + "<answer>"
+            topics = " ".join(item_dict['topics'])
+            line = question_line + item_dict['<answer>'] + " " + "<topic>" + " " + topics
+            line_list.append(line)
+
+    with open(fout, "w", encoding="utf8") as fout:
+        for item in line_list:
+            fout.write(item + "\n")
+
+converter(input_file, output_file)
+
+'''
+    try:
+        for item in loaded_file:
+            print(item, type(item))
+    except json.decoder.JSONDecodeError as e:
+        print(e, item)
+        sys.exit(1)
+'''
